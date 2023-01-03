233816b0493a60f22df27427a5e922bee6ae9921
Max Görner <5477952+MaxG87@users.noreply.github.com>
2023-01-03 08:56:44 +0100

    Return if move was a winning move

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 mod referee;
 
 fn main() {
+    let mut foo = vec![1, 2, 3];
+    let _immut_ref = &foo;
+    let mut _mut_ref = &mut foo;
+    let mut _mut_ref2 = &mut foo;
     println!("Hello, world!");
 }
--- src/referee.rs
+++ src/referee.rs
@@ -1,54 +1,60 @@
-type BoardStateEntry = Option<Player>;
+type BoardStateEntry = Option<PlayerID>;
 type BoardState = Vec<Vec<BoardStateEntry>>;
 
 enum Result {
-    VICTORY,
-    DRAW,
-    DEFEAT,
+    Defeat,
+    Draw,
+    IllegalMove,
+    Victory,
 }
 
 #[derive(PartialEq)]
-struct Player {
+struct PlayerID {
     name: String,
     id: u32,
 }
 
 struct MoveResult<'a> {
     state: &'a BoardState,
-    result: Option<Vec<Result>>,
+    result: Option<Result>,
 }
 
-struct TicTacToeJudge<const N: u32> {
+pub struct TicTacToeJudge<const N: u32> {
     board_state: BoardState,
 }
 
 impl<const N: u32> TicTacToeJudge<N> {
-    pub fn receive_move(&mut self, row: usize, col: usize, player: Player) -> MoveResult {
-        match self.board_state[row][col] {
-            None => {
-                self.board_state[row][col] = Some(player);
-                MoveResult {
-                    state: &self.board_state,
-                    result: None,
-                }
-            }
-            Some(_) => MoveResult {
+    pub fn receive_move(&mut self, row: usize, col: usize, player: PlayerID) -> MoveResult {
+        if let Some(_) = self.board_state[row][col] {
+            MoveResult {
                 state: &self.board_state,
-                result: None,
-            },
+                result: Some(Result::IllegalMove),
+            }
+        } else {
+            self.board_state[row][col] = Some(player);
+            let was_winning_move = self.was_winning_move(row, col, &player);
+            let result: Option<Result> = if was_winning_move {
+                Some(Result::Victory)
+            } else {
+                None
+            };
+            MoveResult {
+                state: &self.board_state,
+                result: result,
+            }
         }
     }
 
-    pub fn was_winning_move(&self, row: usize, col: usize, player: &Player) -> bool {
+    pub fn was_winning_move(&self, row: usize, col: usize, player: &PlayerID) -> bool {
         return self.winning_state_in_row(row, player) || self.winning_state_in_col(col, player);
     }
 
-    pub fn winning_state_in_row(&self, row: usize, player: &Player) -> bool {
+    pub fn winning_state_in_row(&self, row: usize, player: &PlayerID) -> bool {
         let row_it = &self.board_state[row];
         return self.collection_has_winning_state(&mut row_it.iter(), &player);
     }
 
-    pub fn winning_state_in_col(&self, col: usize, player: &Player) -> bool {
+    pub fn winning_state_in_col(&self, col: usize, player: &PlayerID) -> bool {
         let mut column_it = self.board_state.iter().map(|row| &row[col]);
         return self.collection_has_winning_state(&mut column_it, &player);
     }
@@ -56,7 +62,7 @@ impl<const N: u32> TicTacToeJudge<N> {
     pub fn collection_has_winning_state(
         &self,
         collection: &mut dyn Iterator<Item = &BoardStateEntry>,
-        player: &Player,
+        player: &PlayerID,
     ) -> bool {
         let mut counter = 0;
         for elem in collection {
