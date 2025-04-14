175f6a818835dfc17aa881baf1744a49783d6725
Max Görner <5477952+MaxG87@users.noreply.github.com>
2025-04-14 20:25:48 +0200

    Add helper function to iterate 2D arrays

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,30 @@
-use interfaces::*;
-mod interfaces;
+use std::iter::*;
+
+pub fn iter_2d_array<T, const N: usize>(
+    array: [[T; N]; N],
+) -> impl Iterator<Item = (usize, usize, T)> {
+    array
+        .into_iter()
+        .enumerate()
+        .flat_map(|(r, row)| row.into_iter().enumerate().map(move |(c, val)| (r, c, val)))
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_() {
-        let result_vec = vec![(PointPlacement { row: 0, column: 0 }, 0, 0)];
-        let mut result = result_vec.into_iter();
-        assert_eq!(
-            result.next().unwrap(),
-            (PointPlacement { row: 0, column: 0 }, 0, 0)
-        );
+    fn test_iter_2d_array() {
+        const N: usize = 3;
+        let array = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
+        let mut result = iter_2d_array(array);
+        for row in 0..3 {
+            for column in 0..3 {
+                let expected_val = row * N + column;
+
+                assert_eq!(result.next().unwrap(), (row, column, expected_val));
+            }
+        }
+        assert_eq!(result.next(), None);
     }
 }
